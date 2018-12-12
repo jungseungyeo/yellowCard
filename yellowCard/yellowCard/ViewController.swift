f71c2edc917e0bfdff545d260ad416a32af433ca
reo <reo@lezhin.com>
2018-12-12 21:48:03 +0900

    gitignore commit

---
+++ yellowCard/yellowCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yellowCard
+//
+//  Created by 여정승 on 12/12/2018.
+//  Copyright © 2018 linsaeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
