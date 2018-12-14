895a2c2d9572ca8cee926d462b08b33a35f57447
reo <reo@lezhin.com>
2018-12-14 16:12:32 +0900

    파일 정리

---
+++ yellowCard/yellowCard/ViewModel/UserInfoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfoViewModel.swift
+//  yellowCard
+//
+//  Created by 여정승 on 14/12/2018.
+//  Copyright © 2018 linsaeng. All rights reserved.
+//
+
+import Foundation
+
+class UserInfoViewModel {
+    var userInfo: UserInfo?
+}
